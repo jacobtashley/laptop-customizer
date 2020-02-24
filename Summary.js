82e9510c3884be4d95f57dd1f0110d34ca4c99cc
jacobtashley <52290063+jacobtashley@users.noreply.github.com>
2020-02-24 12:11:32 -0800

    Update 1
    Update 2

--- Summary.js
+++ Summary.js
@@ -10,6 +10,20 @@ export default class Summary extends React.Component {
             style: 'currency',
             currency: 'USD'
         });
+        const summary = Object.keys(this.state.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.state.selected[feature];
+      
+            return (
+              <div className="summary__option" key={featureHash}>
+                <div className="summary__option__label">{feature} </div>
+                <div className="summary__option__value">{selectedOption.name}</div>
+                <div className="summary__option__cost">
+                  {USCurrencyFormat.format(selectedOption.cost)}
+                </div>
+              </div>
+            );
+          });
         return (
             <div>
                 <section className="main__summary">
