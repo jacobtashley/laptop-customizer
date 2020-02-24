0b504ac4f790f2e958885abb37bdbe6f1238feaa
jacobtashley <52290063+jacobtashley@users.noreply.github.com>
2020-02-24 12:13:09 -0800

    Update 1

--- Main.js
+++ Main.js
@@ -1,13 +1,15 @@
 import React from 'react' 
 import Form from './Form'
 import Summary from './Summary'
+import Features from './Features';
 
 export default class Main extends React.Component{
     render() {
         return (
             <div>
                 <Form />
-                <Summary />
+                <Summary selected={this.props.selected} />
+                <Features />
             </div>
         )
     }
