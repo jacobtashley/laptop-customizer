9b8b048a920cb512f1349f0d056d84cbbf8e7cd1
jacobtashley <52290063+jacobtashley@users.noreply.github.com>
2020-02-25 09:37:13 -0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,10 @@
+<main>
+          <form className="main__form">
+            <h2>Customize your laptop</h2>
+            <FeatureList  />
+          </form>
+          <section className="main__summary">
+            <h2>Your cart</h2>
+            <Cart />
+          </section>
+        </main>
